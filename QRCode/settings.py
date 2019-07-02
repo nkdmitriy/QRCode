9f056408d230f8650c3bfd18bf8aae24bdbfc247
Dmitriy <nkdm_khv@mail.ru>
2019-07-02 11:05:45 +1000

    first

--- QRCode/settings.py
+++ QRCode/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'QR.apps.QrConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'QRCode.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #'default': {
+	#	 'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+	'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qrcode',
+        'USER': 'root',
+        'PASSWORD': 'Inf0tec$',
+        'HOST': '165.22.127.59',
+        'PORT': '3306',
     }
 }
 
